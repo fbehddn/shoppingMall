9fa13ae692ab0ede346fd88748a7488b3fd7113a
bbjbc <jobc4550@gmail.com>
2023-10-09 22:28:09 +0900

    feat: navbar 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,11 @@
-import "./App.css";
+import Navbar from "./components/Navbar";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <button class="btn btn-secondary">
-        그래그래 동우야 일단 리액트 설치하고 css까지 깔음
-      </button>
-      <h2 className="mt-6 sm:mt-6 md:mt-8 text-center text-zinc-800 text-sm sm:text-sm md:text-md lg:text-lg">
-        Sign in to Task-ing.
-      </h2>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
-}
+};
 
 export default App;

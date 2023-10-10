f4c36ec229930130f89c5e0a6ab0e41936b87c98
bbjbc <jobc4550@gmail.com>
2023-10-11 00:11:35 +0900

    feat: detail 페이지에 상품 정보 받아오기

--- frontend/src/components/Product-detail.js
+++ frontend/src/components/Product-detail.js
@@ -1,21 +1,28 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
-const ProductDetail = (props) => {
+const ProductDetail = () => {
+  const location = useLocation();
+  const productData = location.state.productData;
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <div className="text-center">
-        <img
-          src={props.productData.image}
-          alt="product"
-          className="mx-auto rounded-lg w-1/2"
-        />
-        <h1 className="text-2xl font-semibold mt-4">
-          {props.productData.name}
-        </h1>
-        <h2 className="text-xl font-semibold mt-2">
-          {props.productData.price}
-        </h2>
-        <p className="text-gray-700 mt-2">{props.productData.description}</p>
+    <div className="flex justify-center items-center h-full font-semibold">
+      <div className="container mx-auto p-4 flex items-center">
+        <div className="w-1/2 rounded-lg border-2 border-indigo-600">
+          <img
+            src={productData.image}
+            alt="product"
+            className="mx-auto rounded-lg my-auto"
+          />
+        </div>
+        <div className="w-1/2 ml-40">
+          <h1 className="text-4xl">{productData.name}</h1>
+          <h2 className="text-2xl mt-2">{productData.price}</h2>
+          <p className="mt-2">{productData.description}</p>
+          <button className="mt-4 px-6 py-2 btn btn-outline btn-primary">
+            Cart
+          </button>
+        </div>
       </div>
     </div>
   );

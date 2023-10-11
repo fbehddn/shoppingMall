57fed3c8ae45e574869f190e03712c8a183f67c0
bbjbc <jobc4550@gmail.com>
2023-10-12 00:00:34 +0900

    refactor: detail 버튼 클릭 시 발생하는 오류 수정

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 
 import image from "../assets/chatbot.jpg";
 import image2 from "../assets/dog.jpg";
+import image3 from "../assets/quoka.png";
 
 const Product = () => {
   const navigate = useNavigate();
@@ -21,6 +22,13 @@ const Product = () => {
       description: "사랑스러운 강아지 인형입니다.",
       image: image2,
     },
+    {
+      id: 3,
+      name: "쿼카 인형",
+      price: "25,000원",
+      description: "나는 쿼카임둥",
+      image: image3,
+    },
   ];
 
   const handleDetailClick = (product) => {
@@ -30,7 +38,7 @@ const Product = () => {
   };
 
   return (
-    <div className="flex justify-evenly font-semibold">
+    <div className="flex justify-center font-semibold">
       {productData.map((product) => (
         <div
           className="w-1/5 mx-auto mt-4 p-4 rounded-lg shadow-lg"
@@ -50,7 +58,7 @@ const Product = () => {
             <div className="text-center mt-4">
               <button
                 className="px-6 py-2 btn btn-outline btn-primary"
-                onClick={handleDetailClick}
+                onClick={() => handleDetailClick(product)}
               >
                 Detail
               </button>
